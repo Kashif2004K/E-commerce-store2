011b66094aae4a5a79a57491f18d6951ea74bc8f
Kashif2004K <blackwatch4321@gmail.com>
2026-07-30 14:48:59 +0500

    changes

--- server/index.js
+++ server/index.js
@@ -1,27 +1,56 @@
 import express from "express";
 import { connectDB } from "./config/connectdb.js";
-
+import Product from "./models/product.model.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.use(express.json());
+
+app.get("/", (req, res) => {});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.post("/api/products", (req, res) => {
-  res.send("created product");
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.patch("/api/products/:id", (req, res) => {
-  res.send("product updated");
+app.patch("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.delete("/api/products/:id", (req, res) => {
-  res.send("product deleted");
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.status(200).json("product deleted");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(port, () => {
