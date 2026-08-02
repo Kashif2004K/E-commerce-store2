1d08b1d0b004921a5c4616ac38bee47e532701bf
Kashif2004K <blackwatch4321@gmail.com>
2026-08-03 01:02:51 +0500

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/products">Producst</Link>
       <Link to="/admin">Admin</Link>
